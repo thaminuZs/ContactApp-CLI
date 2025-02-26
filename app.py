ace794ef0084ee89dafa1eca3b4e426d796ce588
thaminuZs <th.sonal@hotmail.com>
2025-02-26 10:26:01 +0530

    Added Functionalities- Edit, Delete, ContactBook

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def get_index(key, value):
 # Search Availability of a Value
 def search_availability(key, value):
     for item in contact_list:
-        return item[key] == value
+        if item[key] == value:
+            return True
     return False
 
 
@@ -44,7 +45,7 @@ def add_contact():
 
 # Search Contact
 def search_contact():
-    name = input("Enter Name For Search : ")
+    name = input("\nEnter Name For Search : ")
     if search_availability("name", name):
         print(
             "\nName : ",
@@ -64,18 +65,70 @@ def search_contact():
 
 # Edit Contacts
 def edit_contact():
-    name = input("Enter Name For Edit : ")
+    name = input("\nEnter Name For Edit : ")
     if search_availability("name", name):
-        # TODO: From Here
+        print(
+            "\nWhat Do You Want To Edit\n1 - Name\n2 - Company\n3 - Phone No\n4 - Email\n"
+        )
+        option = input("Enter Option To Continue : ")
+        if option == "1":
+            contact_list[get_index("name", name)]["name"] = input("\nEnter New Name : ")
+            print("\nUpdated Successfully")
+        elif option == "2":
+            contact_list[get_index("name", name)]["company"] = input(
+                "\nEnter New Company : "
+            )
+            print("\nUpdated Successfully")
+        elif option == "3":
+            contact_list[get_index("name", name)]["phone_no"] = input(
+                "\nEnter New Phone No : "
+            )
+            print("\nUpdated Successfully")
+        elif option == "4":
+            contact_list[get_index("name", name)]["email"] = input(
+                "\nEnter New Email : "
+            )
+            print("\nUpdated Successfully")
+        else:
+            print("\nInvalid Option !")
         menu()
     else:
-        print("Contact Not Found !")
+        print("\nContact Not Found !")
+        menu()
+
+
+# Delete Contact
+def delete_contact():
+    name = input("Enter Name For Delete : ")
+    if search_availability("name", name):
+        del contact_list[get_index("name", name)]
+        print("\nDeleted Successfully")
+        menu()
+    else:
+        print("\nContact Not Found")
+        menu()
+
+
+# View Book
+def view_book():
+    if len(contact_list) == 0:
+        print("\nBook Is Empty !")
+        menu()
+    else:
+        print()
+        print(f"{'Name': <10}{'Company': <10}{'Phone No': <15}{'Email': <20}")
+        print("-" * 55)
+
+        for item in contact_list:
+            print(
+                f"{item['name']: <10}{item['company']: <10}{item['phone_no']: <15}{item['email']: <20}"
+            )
         menu()
 
 
 def menu():
     print(
-        "\n1 - Add Contact\n2 - Search Contact\n3 - Edit Contact\n4- Delete Contact\n5- View Book\n"
+        "\n1 - Add Contact\n2 - Search Contact\n3 - Edit Contact\n4 - Delete Contact\n5 - View Book\n6 - Exit\n"
     )
 
     option = input("Enter Option To Continue : ")
@@ -85,7 +138,17 @@ def menu():
         search_contact()
     elif option == "3":
         edit_contact()
+    elif option == "4":
+        delete_contact()
+    elif option == "5":
+        view_book()
+    elif option == "6":
+        return
+    else:
+        print("\nInvalid Option !")
+        menu()
 
 
 print("====CONTACT APP====")
 menu()
+exit()
